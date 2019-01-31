be7d2f5ad030506b9ad7fed8db2d61c239daa3a1
Henrique Tedeschi <htedeschi@Melaleuca.com>
2019-01-31 10:38:28 -0700

    Handling post requests

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const PORT = process.env.PORT || 5000
 
-// var ejs = require("ejs");
-// const nodemailer = require("nodemailer");
 var express = require("express");
 var bodyParser = require('body-parser');
 
 var app = express();
-// app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// app.set("views", "views");
-// app.set("view engine", "ejs");
 
 
 app.listen(PORT, function () {
@@ -22,8 +17,16 @@ app.listen(PORT, function () {
 
 
 app.get("/", function (req, res) {
-    data = { success: true, request: req, response: res };
-    console.log(data);
     res.send("Page under construction");
+    res.end();
+});
+
+app.post("/whatsapp/message/send", function (req, res) {
+    var obj = { success: true, request: req.body };
+
+    console.log(obj);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.json(obj);
     res.end();
 });
