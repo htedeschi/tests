1f55fb1ecae7b8017e21cb32a9d9bdcf9a2f3a46
Henrique Tedeschi <htedeschi@Melaleuca.com>
2019-01-31 11:51:02 -0700

    Clean code

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const PORT = process.env.PORT || 5000
 var express = require("express");
 var bodyParser = require('body-parser');
 
+// Twilio info
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-
 app.listen(PORT, function () {
     console.log("Listening on port " + PORT);
 });
-
 
 app.get("/", function (req, res) {
     res.send("Page under construction");
@@ -22,15 +24,6 @@ app.get("/", function (req, res) {
 });
 
 app.post("/whatsapp/message/send", function (req, res) {
-    var obj = { success: true, request: req.body, to: req.body.From };
-
-    // Download the helper library from https://www.twilio.com/docs/node/install
-    // Your Account Sid and Auth Token from twilio.com/console
-    const accountSid = process.env.ACCOUNT_SID;
-    const authToken = process.env.AUTH_TOKEN;
-    const client = require('twilio')(accountSid, authToken);
-    const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
     client.messages
         .create({
             body: `Hello there! You sent:"${req.body.Body}"`,
