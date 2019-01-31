39c318308f57c766705c5c3cf37922c440bd937f
Henrique Tedeschi <htedeschi@Melaleuca.com>
2019-01-30 17:05:42 -0700

    Adding files

---
+++ index.js
@@ -0,0 +1,27 @@
+const PORT = process.env.PORT || 5000
+
+// var ejs = require("ejs");
+// const nodemailer = require("nodemailer");
+var express = require("express");
+var bodyParser = require('body-parser');
+
+var app = express();
+// app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// app.set("views", "views");
+// app.set("view engine", "ejs");
+
+
+app.listen(PORT, function () {
+    console.log("Listening on port " + PORT);
+});
+
+
+app.get("/", function (req, res) {
+    res.send("Page under construction");
+    res.end();
+});
